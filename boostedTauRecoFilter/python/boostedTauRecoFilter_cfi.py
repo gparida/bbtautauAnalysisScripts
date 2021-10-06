1ec0af07bf26250d858faf0bbccf19047085a7ab
Andrew David Loeliger <andrew.loeliger@cern.ch>
2021-10-06 16:01:24 -0400

    clean up old directories

--- boostedTauRecoFilter/python/boostedTauRecoFilter_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-boostedTauRecoFilter = cms.EDFilter('boostedTauRecoFilter',
-                                    boostedTauCollection = cms.InputTag("slimmedTausBoosted"),
-                                    tauCollection = cms.InputTag("slimmedTaus")
-)
