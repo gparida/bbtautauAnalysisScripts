32ac8a7d0e71efd9d30a8dfeb7abe563cc2b0b27
Ganesh Parida <ganeshsanu111@gmail.com>
2022-06-24 10:01:24 -0500

    rehashing the resubmit script to check for running Jobs

--- utilities/scripts/resubmitJobGP.py
+++ utilities/scripts/resubmitJobGP.py
@@ -6,38 +6,48 @@ import sys
 import glob
 import argparse
 
+
 def submitJob(samplePath):
     statusDagPath1 = '%s/dag.status' % samplePath
     statusDagPath2 = '%s/dag.dag.status' % samplePath
 
+    print ("statusDagPath1", statusDagPath1)
+
     errors = []
     submitted = []
-    dagStatus = []
+    success = []
     try:
         with open(statusDagPath1,'r') as dagFile:
-            errors = [re.search('STATUS_ERROR', line) for line in dagFile]
+            errors = ["yes" for line in dagFile if (re.search('STATUS_ERROR', line)!=None)]
         with open(statusDagPath1,'r') as dagFile:
-            submitted = [re.search('STATUS_SUBMITTED', line) for line in dagFile]
+            submitted = ["yes"  for line in dagFile if(re.search('STATUS_SUBMITTED', line))!=None]
+        with open(statusDagPath1,'r') as dagFile:
+            success = ["yes"  for line in dagFile if(re.search('(success)', line))!=None]
     except IOError:
         try:
             with open(statusDagPath1,'r') as dagFile:
-                errors = [re.search('STATUS_ERROR', line) for line in dagFile]
+                errors = [re.search('STATUS_ERROR', line)!=None for line in dagFile]
             with open(statusDagPath1,'r') as dagFile:
-                submitted = [re.search('STATUS_SUBMITTED', line) for line in dagFile]
+                submitted = [re.search('STATUS_SUBMITTED', line)!=None for line in dagFile]
+            with open(statusDagPath1,'r') as dagFile:
+                success = [re.search('(success)', line)!=None for line in dagFile]
         except IOError:
             print "\t%s does not seem to be well formatted or have dag status files. Skipping..." %samplePath
             return
+    print ("there are ",len(errors), " errors and ",len(submitted)-1," Jobs running currently")
+    if (any(success) and len(success)==1):
+        print (samplePath, " completed processing fully")
 
-    if any(submitted) and (len(submitted) - 2 > -1):
+    if any(submitted) and (len(submitted)>1):
         print "\t%s is not done. Waiting to resubmit..." % samplePath
         return
 
     if any(errors):
-        print "\t Resubmitting %s..." % samplePath
+        #print "\t Resubmitting %s..." % samplePath
         rescueDag = max(glob.glob('%s/*dag.rescue[0-9][0-9][0-9]' % samplePath))
         print ("file to be submitted: ",rescueDag)
-        #resubmitCommand = 'farmoutAnalysisJobs --rescue-dag-file=%s' % rescueDag
-        #os.system(resubmitCommand)
+        resubmitCommand = 'farmoutAnalysisJobs --rescue-dag-file=%s' % rescueDag
+        os.system(resubmitCommand)
     else:
         pass
 
@@ -54,7 +64,7 @@ def recursivelySearchForDagDir(search):
     theSearch = glob.glob(search)
     results = []
     for item in theSearch:
-        if re.search('.*/dags', item):
+        if re.search('.*/dags$', item):
             results.append(item)
         else:
             results+=recursivelySearchForDagDir(item+'/*')
